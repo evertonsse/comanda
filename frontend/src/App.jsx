8448345845e6a3b28f131823c34d53cc4c97ca89
evertonsse <evertonsse@outlook.com>
2024-03-26 00:51:49 -0300

    start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
-import {  Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import { Layout, Menu } from 'antd'
 
-
+const { Header, Content, Footer } = Layout
 
 const App = () => {
-		return (
-			<>
-				teste
+	return (
+		<Layout style={{ height: '100vh' }}>
+			<Header>
+				<Menu mode="horizontal" items={[{}]}/> 
+			</Header>
+			<Content >
 				<Outlet />
-			</>
-		)
-	}
+			</Content>
+
+			<Footer>Comanda</Footer>
+		</Layout>
+	)
+}
 export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
+import { ThemeProvider } from 'antd-style'
 
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import Login from './pages/Login/Login.jsx'
-import {  AuthProvider } from './context/AuthContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 import { PrivateRoute } from './privateRoutes.jsx'
-
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <PrivateRoute/>,
+		element: <PrivateRoute />,
 		children: [
 			{
 				path: 'home',
@@ -28,7 +29,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<AuthProvider>
-			<RouterProvider router={router} />
+			<ThemeProvider  themeMode={'auto'}>
+				<RouterProvider router={router} />
+			</ThemeProvider>
 		</AuthProvider>
 	</React.StrictMode>
 )
